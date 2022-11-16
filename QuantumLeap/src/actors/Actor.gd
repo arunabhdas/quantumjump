3d74d568a3887f3edac3f02c9231597794202421
Arunabh Das <arunabhdas@gmail.com>
2022-11-16 14:40:00 -0500

    Improved assets

--- QuantumLeap/src/actors/Actor.gd
+++ QuantumLeap/src/actors/Actor.gd
@@ -1,10 +1,11 @@
 extends KinematicBody2D
 
 class_name Actor
+const FLOOR_NORMAL = Vector2.UP
 export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 500.0
-var velocity: = Vector2.ZERO
+var _velocity: = Vector2.ZERO
 
 # func _physics_process(delta: float) -> void:
-	# velocity.y += gravity * delta
-	# velocity = move_and_slide(velocity)
+	# _velocity.y += gravity * delta
+	# _velocity = move_and_slide(_velocity)
