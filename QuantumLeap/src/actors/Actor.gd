04c084e4c5fdef138842f87e98846243d3086485
Arunabh Das <arunabhdas@gmail.com>
2022-11-16 00:30:02 -0500

    Improved physics of Player

--- QuantumLeap/src/actors/Actor.gd
+++ QuantumLeap/src/actors/Actor.gd
@@ -5,6 +5,6 @@ export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 500.0
 var velocity: = Vector2.ZERO
 
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity)
+# func _physics_process(delta: float) -> void:
+	# velocity.y += gravity * delta
+	# velocity = move_and_slide(velocity)
